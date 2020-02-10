ac2d2f002a5525d4b24d3abc8d34b7e7ea9ac328
Erkka Rahikainen <ergga96@gmail.com>
2020-02-10 18:27:47 +0200

    heroku

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+app.use(express.json())
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
+morgan.token('data', function (req) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
+
+
+let persons = [
+  { name: 'Arto Hellas', number: '040-123456',id:0},
+  { name: 'Ada Lovelace', number: '39-44-5323523',id:1},
+  { name: 'Dan Abramov', number: '12-43-234345',id:2},
+  { name: 'Mary Poppendieck', number: '39-23-6423122',id:3}
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/info', (req, res) => {
+  const info = `Phonebook contains ${persons.length} contacts.</br>${new Date()}`
+  res.send(info)
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
-const PORT = 3001
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  person ? res.json(person):res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+
+  const body = req.body;
+
+  body.name && body.number ||  res.status(400).json({error: 'content mia'}).end()
+
+  persons.filter(person => person.name === body.name).length
+  && res.status(400).json({error: 'Name must be unique'}).end()
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+const generateId = () => {
+  return Math.floor(Math.random()*99999999+4)
+}
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
